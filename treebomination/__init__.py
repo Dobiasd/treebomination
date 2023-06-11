3d68c04237cf5b67aa14f0127245af15713214e1
Dobiasd <editgym@gmail.com>
2023-06-11 17:13:28 +0200

    Add __init__.py

---
+++ treebomination/__init__.py
@@ -0,0 +1,9 @@
+from ._conversion import treebominate as treebominate
+
+name = "treebomination"
+
+__author__ = "Tobias Hermann"
+__copyright__ = "Copyright 2023, Tobias Hermann"
+__email__ = "editgym@gmail.com"
+__license__ = "MIT"
+__version__ = "0.1.0"
